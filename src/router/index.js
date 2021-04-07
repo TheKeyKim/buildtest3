a26290aecaa625520ccd5893415bd6e2166e5996
TheKey <thekey@TheKeyui-MacBookPro.local>
2021-04-07 20:02:12 +0900

    TT

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Problemset from '../views/Problemset.vue'
-import Login from '../views/Login.vue'
-import Signin from '../views/Signin.vue'
 
 Vue.use(VueRouter)
 
@@ -12,21 +9,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/problemset',
-    name: 'prblemset',
-    component: Problemset
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/signin',
-    name: 'Signin',
-    component: Signin
   }
 ]
 
